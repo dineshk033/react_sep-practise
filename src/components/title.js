b02573d8942171919e01bc427e61ac8676dd0d27
dinesh <dineshk033@gmail.com>
2023-11-27 08:56:09 +0530

    Component title message changeg

--- src/components/title.js
+++ src/components/title.js
@@ -2,7 +2,8 @@ const Title = () => {
   return (
     <div>
       <h1>Welcome to React</h1>
-      <h5>Tech session</h5>
+      <h5>Tech session </h5>
+      <h5>Git session </h5>
     </div>
   );
 };
