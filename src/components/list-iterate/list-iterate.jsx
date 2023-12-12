6910a2f153add8992648960acca16f8bac96e120
dinesh <dineshk033@gmail.com>
2023-12-12 09:22:00 +0530

    Event handling

--- src/components/list-iterate/list-iterate.jsx
+++ src/components/list-iterate/list-iterate.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import { MOCKDATA } from "../../data";
 
 const ListItem = ({ item, index }) => {
+  console.log(item);
+  function handleAdd(e, arg) {
+    e.stopPropagation();
+    alert(arg);
+  }
+
+  function handleCard() {
+    alert("card....");
+  }
   return (
     <>
-      <div className="m-3 p-2 card flex-row">
+      <div onClick={handleCard} className="m-3 p-2 card flex-row">
         {index + 1 + ". " + item.title}
         <span className="px-3 text-danger">{item.category}</span>
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={(e) => handleAdd(e, item.title)}
+        >
+          + Add to cart
+        </button>
       </div>
     </>
   );
