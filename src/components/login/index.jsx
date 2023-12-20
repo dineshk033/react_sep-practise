951c2006e8539e8a5620ef9368e42e567bb60a1e
dinesh <dineshk033@gmail.com>
2023-12-20 09:14:26 +0530

    validate

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,14 +1,55 @@
 import React, { useState } from "react";
+import { passwordMatch } from "../../shared";
 
 export default function Login() {
-  const [userName, setUserName] = useState("xx@gmail.com");
-  const [password, setPassword] = useState("");
-  function handleLogin() {
-    console.log(password, userName);
+  const [form, setForm] = useState({
+    userName: "",
+    password: "",
+  });
+  const [error, setError] = useState({ userName: "", password: "" });
+  // const [userName, setUserName] = useState("xx@gmail.com");
+  // const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+    if (!form.userName) {
+      setError({ ...error, userName: "Username is requierd" });
+    }
+    if (!passwordMatch(form.password)) {
+      setError({ ...error, password: "Password doesn't meet requirement" });
+    }
+    console.log(form.password, form.userName);
   }
+  function handleChange(e) {
+    // console.log(e.target.name);
+    const key = e.target.name;
+    const value = e.target.value;
+    switch (key) {
+      case "userName":
+        if (value) {
+          setError({ ...error, userName: "" });
+        }
+        // setForm({ ...form, [key]: e.target.value });
+        break;
+      default:
+        if (passwordMatch(form.password)) {
+          setError({ ...error, password: "" });
+        }
+        break;
+    }
+    setForm({ ...form, [key]: e.target.value });
+  }
+
+  function handleReset() {
+    setForm({
+      userName: "",
+      password: "",
+    });
+  }
+  // console.log(form);
   return (
     <div className="row  justify-content-center">
-      <div className="col-md-5 card p-3">
+      <form className="col-md-5 card p-3">
         <h4>Login</h4>
         <hr />
         <div className="mb-3">
@@ -19,13 +60,14 @@ export default function Login() {
             type="email"
             className="form-control"
             id="exampleFormControlInput1"
-            value={userName}
+            value={form.userName}
+            name="userName"
             placeholder="name@example.com"
-            onChange={(e) => {
-              console.log(e);
-              setUserName(e.target.value);
-            }}
+            onChange={handleChange}
           />
+          {error.userName && (
+            <small className="text-danger">Username is required</small>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
@@ -33,16 +75,27 @@ export default function Login() {
           </label>
           <input
             type="password"
-            defaultValue={password}
+            defaultValue={form.password}
+            name="password"
             className="form-control"
             id="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handleChange}
           />
+          {error.password && (
+            <small className="text-danger">{error.password}</small>
+          )}
         </div>
         <button className="btn btn-primary" onClick={handleLogin}>
           Login
+        </button>{" "}
+        <button
+          className="btn btn-warning d-block mt-2"
+          type="reset"
+          onClick={handleReset}
+        >
+          Reset
         </button>
-      </div>
+      </form>
     </div>
   );
 }
---
+++ src/shared/index.js
@@ -0,0 +1,5 @@
+export function passwordMatch(arg) {
+  const pattern =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/gm;
+  return pattern.test(arg);
+}
